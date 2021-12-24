4a44f5ad8be3bc876c96c500ad6b2dcd4892d36b
Linden <70739041+linden@users.noreply.github.com>
2021-12-24 03:12:49 -0800

    test: add examples

---
+++ examples/echo_client.rs
@@ -0,0 +1,19 @@
+use solitude;
+
+fn main() {
+	let arguments: Vec<String> = std::env::args().collect();
+
+	if arguments.len() < 2 {
+		panic!("must supply I2P hostname, i.e. eva example.i2p");
+	}
+
+	let hostname = arguments[1].clone();
+	
+	println!("{}", hostname);
+	
+	let mut tunnel = solitude::Tunnel::new("echo_client".to_string()).unwrap();
+	
+	let destination = tunnel.look_up(hostname).unwrap();
+
+	tunnel.send_to(destination, String::from("Hello World")).unwrap();
+}
