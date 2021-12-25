0829484ed92ef42344ca92c663c914486f353ee2
Ellis Frank <mooseman1241@protonmail.com>
2021-12-25 11:12:47 -0600

    feat: adding logging with configurable logging level

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use std::net::UdpSocket;
 #[macro_use]
 extern crate anyhow;
 
+#[macro_use]
+extern crate log;
+
 use anyhow::{Context, Result};
 use data_encoding::{Specification, BASE32};
 use sha2::{Digest, Sha256};
@@ -23,42 +26,49 @@ pub struct Tunnel {
 }
 
 impl Tunnel {
-	fn command(&mut self, command: &str) -> Result<String> {
-		self.stream.write_all(command.as_bytes())?;
+	pub fn new(service: String) -> Result<Tunnel> {
+        trace!("creating new tunnel with ID {}", service);
 
-		let mut response = String::new();
+		let stream = TcpStream::connect("localhost:7656").context("couldn't connect to local SAM bridge")?;
 
-		let _ = self.reader.read_line(&mut response)?;
+		let socket = UdpSocket::bind(("0.0.0.0", 0))?;
+		let port = socket.local_addr()?.port();
 
-		Ok(response)
-	}
+		let mut connection = Tunnel {
+			reader: BufReader::new(stream.try_clone()?),
+			stream,
+			socket,
+			public_key: String::new(),
+			private_key: String::new(),
+			service,
+		};
 
-	pub fn look_up(&mut self, address: String) -> Result<String> {
-		let expression = regex::Regex::new(r#"NAMING REPLY RESULT=OK NAME=([^ ]*) VALUE=(?P<value>[^\n]*)\n"#)?;
+		connection.hello()?;
 
-		let body = self.command(&format!("NAMING LOOKUP NAME={}\n", address))?;
+		connection.keys()?;
 
-		let matches = expression.captures(&body).context("could not resolve domain")?;
-		let value = matches
-			.name("value")
-			.context("no return value, possibly an invalid domain")?
-			.as_str()
-			.to_string();
+		connection.new_session(port)?;
 
-		Ok(value)
+        info!("Created new SAMv3 session {:?}", connection);
+
+		Ok(connection)
 	}
 
 	fn hello(&mut self) -> Result<()> {
+        trace!("sam connection with ID {} executed hello", self.service);
+
 		let expression = regex::Regex::new(r#"HELLO REPLY RESULT=OK VERSION=(.*)\n"#)?;
 
 		if !expression.is_match(&self.command("HELLO VERSION MIN=3.0 MAX=3.2\n")?) {
-			bail!("didn't receive a hello response from i2p")
+			bail!("didn't receive a hello response from i2p");
 		}
 
 		Ok(())
 	}
 
 	fn keys(&mut self) -> Result<()> {
+        trace!("sam connection with ID {} got keys", self.service);
+
 		let expression = regex::Regex::new(r#"DEST REPLY PUB=(?P<public>[^ ]*) PRIV=(?P<private>[^\n]*)"#)?;
 
 		let body = &self.command("DEST GENERATE\n")?;
@@ -71,6 +81,8 @@ impl Tunnel {
 	}
 
 	fn new_session(&mut self, port: u16) -> Result<()> {
+        trace!("sam connection with ID {} made a new session", self.service);
+
 		let expression = regex::Regex::new(r#"SESSION STATUS RESULT=OK DESTINATION=([^\n]*)"#)?;
 
 		let body = &self.command(&format!(
@@ -97,11 +109,15 @@ impl Tunnel {
 		Ok(address.trim_end_matches('=').to_owned() + ".b32.i2p")
 	}
 
-	pub fn close(&self) {
-		todo!("tunnel closing");
+	pub fn close(&mut self) -> Result<()> {
+        self.command(&format!("SESSION REMOVE ID={}", self.service))?;
+
+        Ok(())
 	}
 
 	pub fn send_to(&mut self, address: String, message: String) -> Result<usize> {
+        trace!("sam connection with ID {} is sending data to {}", self.service, address);
+
 		let length = &self.socket.send_to(
 			format!("3.1 {} {}\n{}", &self.service, address, message).as_bytes(),
 			"127.0.0.1:7655",
@@ -110,27 +126,34 @@ impl Tunnel {
 		Ok(*length)
 	}
 
-	pub fn new(service: String) -> Result<Tunnel> {
-		let stream = TcpStream::connect("localhost:7656").context("couldn't connect to local SAM bridge")?;
+	fn command(&mut self, command: &str) -> Result<String> {
+        trace!("sam connection with ID {} is executing command {}", self.service, command);
 
-		let socket = UdpSocket::bind(("0.0.0.0", 0))?;
-		let port = socket.local_addr()?.port();
+		self.stream.write_all(command.as_bytes())?;
 
-		let mut connection = Tunnel {
-			reader: BufReader::new(stream.try_clone()?),
-			stream,
-			socket,
-			public_key: String::new(),
-			private_key: String::new(),
-			service,
-		};
+		let mut response = String::new();
 
-		connection.hello()?;
-		connection.keys()?;
+		let _ = self.reader.read_line(&mut response)?;
 
-		connection.new_session(port)?;
+		Ok(response)
+	}
 
-		Ok(connection)
+	pub fn look_up(&mut self, address: String) -> Result<String> {
+        trace!("sam connection with ID {} is looking up address {}", self.service, address);
+
+		let expression = regex::Regex::new(r#"NAMING REPLY RESULT=OK NAME=([^ ]*) VALUE=(?P<value>[^\n]*)\n"#)?;
+
+		let body = self.command(&format!("NAMING LOOKUP NAME={}\n", address))?;
+
+		let matches = expression.captures(&body).context("could not resolve domain")?;
+
+		let value = matches
+			.name("value")
+			.context("no return value, possibly an invalid domain")?
+			.as_str()
+			.to_string();
+
+		Ok(value)
 	}
 }
 
