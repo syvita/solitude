8a8210dbfdc852e6e2f88d8ea010688364fed4a1
MooseMan <mooseman1241@protonmail.com>
2021-12-25 11:12:47 -0600

    feat: adding logging with configurable logging level

--- tests/test.rs
+++ tests/test.rs
@@ -1,23 +1,31 @@
-use std::thread;
-
 use solitude::Tunnel;
 
 use anyhow::{Context, Result};
 
+fn init() -> Result<()> {
+    env_logger::builder().is_test(true).try_init()?;
+    Ok(())
+}
+
 #[test]
 fn can_create_tunnel() -> Result<()> {
+    init()?;
+
 	let test_name = "can_create_tunnel".to_string();
 
-	let _tunnel = Tunnel::new(test_name).context("Couldn't create tunnel. Is i2pd or i2prouter running? Make sure SAMv3 is enabled.")?;
+	let mut tunnel = Tunnel::new(test_name)?;
 
+    tunnel.close()?;
 	Ok(())
 }
 
 #[test]
 fn service_can_be_resolved() -> Result<()> {
+    init()?;
+
 	let test_name = "can_execute_hello".to_string();
 
-	let tunnel = Tunnel::new(test_name.clone())?;
+	let mut tunnel = Tunnel::new(test_name.clone())?;
 
 	let test_child_name = [test_name, "_child".to_string()].concat();
 	let mut new_tunnel = Tunnel::new(test_child_name)?;
@@ -26,12 +34,17 @@ fn service_can_be_resolved() -> Result<()> {
 	let name = new_tunnel.look_up(tunnel_address.clone())?;
 	println!("resolved {} to {}", tunnel_address, name);
 
+    tunnel.close()?;
+    new_tunnel.close()?;
+
 	Ok(())
 }
 
 #[test]
 fn can_send_data_to_service() -> Result<()> {
-	let test_name = "can_connect_to_service".to_string();
+    init()?;
+
+	let test_name = "can_send_data_to_service".to_string();
 
 	let mut tunnel = Tunnel::new(test_name.clone())?;
 	let destination = tunnel.look_up("ME".to_string())?;
@@ -40,6 +53,9 @@ fn can_send_data_to_service() -> Result<()> {
 	let mut new_tunnel = Tunnel::new(test_child_name)?;
 
 	new_tunnel.send_to(destination, "Hello".to_string())?;
+
+    tunnel.close()?;
+    new_tunnel.close()?;
 
 	Ok(())
 }
