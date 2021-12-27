40e05eb5e6e779a4a68ac5b9a6b745520c2e402c
Linden <70739041+linden@users.noreply.github.com>
2021-12-27 02:22:27 -0800

    style: `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ pub struct Session {
 
 impl Session {
 	pub fn new(service: String, forwarding_address: &str, forwarding_port: u16) -> Result<Self> {
-        debug!("creating new session with ID {}", service);
+		debug!("creating new session with ID {}", service);
 
 		let stream = TcpStream::connect("localhost:7656").context("couldn't connect to local SAM bridge")?;
 
@@ -43,13 +43,13 @@ impl Session {
 
 		session.bridge(forwarding_address, forwarding_port)?;
 
-        info!("Created new SAMv3 session {:?}", session);
+		info!("Created new SAMv3 session {:?}", session);
 
 		Ok(session)
 	}
 
 	fn hello(&mut self) -> Result<()> {
-        debug!("sam connection with ID {} executed hello", self.service);
+		debug!("sam connection with ID {} executed hello", self.service);
 
 		let expression = regex::Regex::new(r#"HELLO REPLY RESULT=OK VERSION=(.*)\n"#)?;
 
@@ -61,7 +61,7 @@ impl Session {
 	}
 
 	fn keys(&mut self) -> Result<()> {
-        debug!("sam connection with ID {} got keys", self.service);
+		debug!("sam connection with ID {} got keys", self.service);
 
 		let expression = regex::Regex::new(r#"DEST REPLY PUB=(?P<public>[^ ]*) PRIV=(?P<private>[^\n]*)"#)?;
 
@@ -75,7 +75,7 @@ impl Session {
 	}
 
 	fn bridge(&mut self, forwarding_address: &str, port: u16) -> Result<()> {
-        debug!("sam connection with ID {} made a new session", self.service);
+		debug!("sam connection with ID {} made a new session", self.service);
 
 		let expression = regex::Regex::new(r#"SESSION STATUS RESULT=OK DESTINATION=([^\n]*)"#)?;
 
@@ -104,36 +104,41 @@ impl Session {
 	}
 
 	pub fn close(&mut self) -> Result<()> {
-        debug!("sam connection with ID {} is closing session", self.service);
+		debug!("sam connection with ID {} is closing session", self.service);
 
-        self.command_with_no_response("QUIT")?;
+		self.command_with_no_response("QUIT")?;
 
-        Ok(())
+		Ok(())
 	}
 
-    fn command_with_no_response(&mut self, command: &str) -> Result<()> {
-        debug!("sam connection with ID {} is executing command {}", self.service, command);
+	fn command_with_no_response(&mut self, command: &str) -> Result<()> {
+		debug!("sam connection with ID {} is executing command {}", self.service, command);
 
-        self.stream.write_all(command.as_bytes())?;
+		self.stream.write_all(command.as_bytes())?;
 
-        Ok(())
-    }
+		Ok(())
+	}
 
 	fn command(&mut self, command: &str) -> Result<String> {
-        debug!("sam connection with ID {} is executing command {}", self.service, command);
+		debug!("sam connection with ID {} is executing command {}", self.service, command);
 
 		self.stream.write_all(command.as_bytes())?;
 
 		let mut response = String::new();
 
 		self.reader.read_line(&mut response)?;
-        trace!("sam connection with ID {} sent command {} and got response {}", self.service, command, response);
+		trace!(
+			"sam connection with ID {} sent command {} and got response {}",
+			self.service,
+			command,
+			response
+		);
 
 		Ok(response)
 	}
 
 	pub fn look_up(&mut self, address: String) -> Result<String> {
-        debug!("sam connection with ID {} is looking up address {}", self.service, address);
+		debug!("sam connection with ID {} is looking up address {}", self.service, address);
 
 		let expression = regex::Regex::new(r#"NAMING REPLY RESULT=OK NAME=([^ ]*) VALUE=(?P<value>[^\n]*)\n"#)?;
 
@@ -153,54 +158,62 @@ impl Session {
 
 #[derive(Debug, PartialEq)]
 pub struct DatagramMessage {
-    pub session_id: String,
-    pub destination: String,
-    pub contents: Vec<u8>,
+	pub session_id: String,
+	pub destination: String,
+	pub contents: Vec<u8>,
 }
 
 impl DatagramMessage {
-    pub fn new(session_id: &str, destination: &str, contents: Vec<u8>) -> Self {
-        Self {
-            session_id: session_id.to_owned(),
-            destination: destination.to_owned(),
-            contents
-        }
-    }
+	pub fn new(session_id: &str, destination: &str, contents: Vec<u8>) -> Self {
+		Self {
+			session_id: session_id.to_owned(),
+			destination: destination.to_owned(),
+			contents,
+		}
+	}
 
-    pub fn serialize(&self) -> Vec<u8> {
-        trace!("serializing datagram message");
+	pub fn serialize(&self) -> Vec<u8> {
+		trace!("serializing datagram message");
 
-        let header = format!("3.0 {} {}\n", self.session_id, self.destination);
-        let mut bytes = header.as_bytes().to_vec();
-        bytes.append(&mut self.contents.clone());
+		let header = format!("3.0 {} {}\n", self.session_id, self.destination);
+		let mut bytes = header.as_bytes().to_vec();
+		bytes.append(&mut self.contents.clone());
 
-        bytes
-    }
+		bytes
+	}
 
-    pub fn from_bytes(session_id: &str, buffer: &[u8]) -> Result<Self> {
-        trace!("deserializing datagram message");
+	pub fn from_bytes(session_id: &str, buffer: &[u8]) -> Result<Self> {
+		trace!("deserializing datagram message");
 
-        // Split the buffer, using the first 0x0a (newline) byte as the delimiter
-        let split_buffer: Vec<&[u8]> = buffer.splitn(2, |byte| *byte == 0x0a).collect();
+		// Split the buffer, using the first 0x0a (newline) byte as the delimiter
+		let split_buffer: Vec<&[u8]> = buffer.splitn(2, |byte| *byte == 0x0a).collect();
 
-        let header_bytes = split_buffer.iter().nth(0).context("Cannot deserialize an empty buffer")?;
+		let header_bytes = split_buffer.iter().nth(0).context("Cannot deserialize an empty buffer")?;
 
-        let header = String::from_utf8(header_bytes.to_vec())?;
+		let header = String::from_utf8(header_bytes.to_vec())?;
 
-        let expression = regex::Regex::new(r#"DATAGRAM RECEIVED DESTINATION=(?P<destination>[^ ]*) SIZE=(?P<size>[^\n]*)"#)?;
+		let expression = regex::Regex::new(r#"DATAGRAM RECEIVED DESTINATION=(?P<destination>[^ ]*) SIZE=(?P<size>[^\n]*)"#)?;
 
-        let matches = expression.captures(&header).context("Could not find fields in received datagram")?;
+		let matches = expression.captures(&header).context("Could not find fields in received datagram")?;
 
-        let destination = matches.name("destination").context("Could not find destination in received datagram")?.as_str().to_owned();
+		let destination = matches
+			.name("destination")
+			.context("Could not find destination in received datagram")?
+			.as_str()
+			.to_owned();
 
-        let contents = split_buffer.iter().nth(1).context("could not find contents of datagram message")?.to_vec();
+		let contents = split_buffer
+			.iter()
+			.nth(1)
+			.context("could not find contents of datagram message")?
+			.to_vec();
 
-        Ok(Self {
-            session_id: session_id.to_owned(),
-            destination,
-            contents,
-        })
-    }
+		Ok(Self {
+			session_id: session_id.to_owned(),
+			destination,
+			contents,
+		})
+	}
 }
 
 fn decode_base_64(base_64_code: &str) -> Result<Vec<u8>> {
--- tests/test.rs
+++ tests/test.rs
@@ -1,117 +1,114 @@
-use solitude::{Session, DatagramMessage};
+use solitude::{DatagramMessage, Session};
 
 use std::net::UdpSocket;
 
 use anyhow::{Context, Result};
 
 fn init() {
-    let _ = env_logger::builder().is_test(true).format_module_path(true).try_init();
+	let _ = env_logger::builder().is_test(true).format_module_path(true).try_init();
 }
 
-
-
 #[test]
-fn can_create_session () -> Result<()> {
-    init();
+fn can_create_session() -> Result<()> {
+	init();
 
 	let test_name = "can_create_session ".to_string();
 
 	let mut session = Session::new(test_name, "0.0.0.0", 0)?;
 
-    session.close()?;
+	session.close()?;
 	Ok(())
 }
 
 #[test]
 fn service_can_be_resolved() -> Result<()> {
-    init();
+	init();
 
 	let test_name = "service_can_be_resolved";
 
-    let (mut session, mut second_session) = create_two_sessions(test_name, 0, 0)?;
+	let (mut session, mut second_session) = create_two_sessions(test_name, 0, 0)?;
 
 	let session_address = session.address()?;
 	let name = second_session.look_up(session_address.clone())?;
 	println!("resolved {} to {}", session_address, name);
 
-    session.close()?;
-    second_session.close()?;
+	session.close()?;
+	second_session.close()?;
 
 	Ok(())
 }
 
 #[test]
 fn can_send_datagram_to_service() -> Result<()> {
-    init();
+	init();
 
 	let test_name = "can_send_data_to_service".to_string();
 
-    let (udp_socket, second_udp_socket) = create_two_udp_sockets()?;
+	let (udp_socket, second_udp_socket) = create_two_udp_sockets()?;
 
-    let (mut session, mut second_session) = create_two_sessions(&test_name, udp_socket.local_addr()?.port(), second_udp_socket.local_addr()?.port())?;
-
+	let (mut session, mut second_session) =
+		create_two_sessions(&test_name, udp_socket.local_addr()?.port(), second_udp_socket.local_addr()?.port())?;
 
 	let destination = second_session.look_up("ME".to_string())?;
 
-    let datagram_message = DatagramMessage::new(&test_name, &destination, [0x05, 0x15].to_vec());
-    let datagram_message_bytes = datagram_message.serialize();
+	let datagram_message = DatagramMessage::new(&test_name, &destination, [0x05, 0x15].to_vec());
+	let datagram_message_bytes = datagram_message.serialize();
 
-    udp_socket.send(&datagram_message_bytes)?;
+	udp_socket.send(&datagram_message_bytes)?;
 
-    session.close()?;
-    second_session.close()?;
+	session.close()?;
+	second_session.close()?;
 
 	Ok(())
 }
 
 #[test]
 fn can_create_datagram_message() -> Result<()> {
-    init();
+	init();
 
-    let contents: [u8; 32] = rand::random();
-    let datagram_message = DatagramMessage::new("test", "test_destination", contents.to_vec());
+	let contents: [u8; 32] = rand::random();
+	let datagram_message = DatagramMessage::new("test", "test_destination", contents.to_vec());
 
-    Ok(())
+	Ok(())
 }
 
 #[test]
 fn can_serialize_datagram_message() -> Result<()> {
-    init();
+	init();
 
-    let contents: [u8; 32] = rand::random();
-    let datagram_message = DatagramMessage::new("test", "test_destination", contents.to_vec());
-    let _datagram_message_bytes = datagram_message.serialize();
+	let contents: [u8; 32] = rand::random();
+	let datagram_message = DatagramMessage::new("test", "test_destination", contents.to_vec());
+	let _datagram_message_bytes = datagram_message.serialize();
 
-    Ok(())
+	Ok(())
 }
 
 #[test]
 fn can_deserialize_datagram_message() -> Result<()> {
-    init();
+	init();
 
-    let example_received_datagram_bytes = [
-        0x44, 0x41, 0x54, 0x41, 0x47, 0x52, 0x41, 0x4d, 0x20, 0x52, 0x45, 0x43, 0x45, 0x49, 0x56,
-        0x45, 0x44, 0x20, 0x44, 0x45, 0x53, 0x54, 0x49, 0x4e, 0x41, 0x54, 0x49, 0x4f, 0x4e, 0x3d,
-        0x64, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x53, 0x49, 0x5a,
-        0x45, 0x3d, 0x6e, 0x75, 0x6d, 0x42, 0x79, 0x74, 0x65, 0x73, 0x0a, 0x05, 0x10
-    ];
+	let example_received_datagram_bytes = [
+		0x44, 0x41, 0x54, 0x41, 0x47, 0x52, 0x41, 0x4d, 0x20, 0x52, 0x45, 0x43, 0x45, 0x49, 0x56, 0x45, 0x44, 0x20, 0x44, 0x45, 0x53, 0x54,
+		0x49, 0x4e, 0x41, 0x54, 0x49, 0x4f, 0x4e, 0x3d, 0x64, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x53, 0x49,
+		0x5a, 0x45, 0x3d, 0x6e, 0x75, 0x6d, 0x42, 0x79, 0x74, 0x65, 0x73, 0x0a, 0x05, 0x10,
+	];
 
-    let datagram_message_after_deserialization = DatagramMessage::from_bytes("test", &example_received_datagram_bytes)?;
+	let datagram_message_after_deserialization = DatagramMessage::from_bytes("test", &example_received_datagram_bytes)?;
 
-    assert_eq!(datagram_message_after_deserialization.contents, [0x05, 0x10].to_vec());
-    assert_eq!(datagram_message_after_deserialization.destination, "destination");
+	assert_eq!(datagram_message_after_deserialization.contents, [0x05, 0x10].to_vec());
+	assert_eq!(datagram_message_after_deserialization.destination, "destination");
 
-    Ok(())
+	Ok(())
 }
 
 fn create_two_udp_sockets() -> Result<(UdpSocket, UdpSocket)> {
-    let first_udp_socket = UdpSocket::bind("0.0.0.0:0")?;
-    let second_udp_socket = UdpSocket::bind("0.0.0.0:0")?;
+	let first_udp_socket = UdpSocket::bind("0.0.0.0:0")?;
+	let second_udp_socket = UdpSocket::bind("0.0.0.0:0")?;
 
-    first_udp_socket.connect("127.0.0.1:7655")?;
-    second_udp_socket.connect("127.0.0.1:7655")?;
+	first_udp_socket.connect("127.0.0.1:7655")?;
+	second_udp_socket.connect("127.0.0.1:7655")?;
 
-    Ok((first_udp_socket, second_udp_socket))
+	Ok((first_udp_socket, second_udp_socket))
 }
 
 fn create_two_sessions(test_name: &str, first_port: u16, second_port: u16) -> Result<(Session, Session)> {
@@ -120,5 +117,5 @@ fn create_two_sessions(test_name: &str, first_port: u16, second_port: u16) -> Re
 	let test_child_name = [test_name.to_owned(), "_child".to_string()].concat();
 	let second_session = Session::new(test_child_name, "0.0.0.0", second_port)?;
 
-    Ok((session, second_session))
+	Ok((session, second_session))
 }
