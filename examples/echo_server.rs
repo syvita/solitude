e6528a21208e2be69f727ee78370a7c3a00bef43
Linden <70739041+linden@users.noreply.github.com>
2021-12-27 03:45:40 -0800

    fix!: update examples
    note: currently cannot get this to work, pretty sure it's up to spec though.

--- examples/echo_server.rs
+++ examples/echo_server.rs
@@ -1,18 +1,24 @@
+use std::net::UdpSocket;
+
 use solitude::Session;
 
 fn main() {
-	let session = Session::new(String::from("echo_server")).expect("couldn't create session");
+	let socket = UdpSocket::bind("0.0.0.0:0").expect("failed to bind");
+	
+	let port = socket.local_addr().expect("failed to get address").port();
+	
+	let session = Session::new(String::from("echo_server"), "0.0.0.0", port).expect("couldn't create session");
 
-	println!("listening at {}", session.address().unwrap());
+	println!("listening at 0.0.0.0:{} or {}", port, session.address().unwrap());
 
 	let mut buffer = [0; 2048];
 
 	loop {
-		let (frame, source) = session.socket.recv_from(&mut buffer).unwrap();
+		let (frame, source) = socket.recv_from(&mut buffer).unwrap();
 		let buffer = &mut buffer[..frame];
 
 		println!("from: {:?}, buffer: {:?}", source, buffer);
 
-		session.socket.send_to(buffer, &source).unwrap();
+		socket.send_to(buffer, &source).unwrap();
 	}
 }
