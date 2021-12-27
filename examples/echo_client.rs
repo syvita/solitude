547a06f8ba692c31e20f30f6b51db631226ceaf3
Linden <70739041+linden@users.noreply.github.com>
2021-12-27 03:45:40 -0800

    fix!: update examples
    note: currently cannot get this to work, pretty sure it's up to spec though.

--- examples/echo_client.rs
+++ examples/echo_client.rs
@@ -1,4 +1,6 @@
-use solitude::Session;
+use std::net::UdpSocket;
+
+use solitude::{Session, DatagramMessage};
 
 fn main() {
 	let arguments: Vec<String> = std::env::args().collect();
@@ -9,11 +11,16 @@ fn main() {
 
 	let hostname = arguments[1].clone();
 
-	println!("{}", hostname);
+	let socket = UdpSocket::bind("0.0.0.0:0").expect("failed to bind");
+		
+	socket.connect("127.0.0.1:7655").expect("failed to connect to I2P's UDP bridge");
+	
+	let mut session = Session::new("echo_client".to_string(), "0.0.0.0", 0).expect("failed to create Session");
 
-	let mut session = Session::new("echo_client".to_string()).unwrap();
-
-	let destination = session.look_up(hostname).unwrap();
-
-	session.send_to(destination, String::from("Hello World")).unwrap();
+	let destination = session.look_up(hostname).expect("failed to lookup hostname");
+	
+	let message = DatagramMessage::new("echo_client", &destination, String::from("Hello World").into_bytes());
+	let raw = message.serialize();
+	
+	socket.send(&raw).expect("failed to send message");
 }
