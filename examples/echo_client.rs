9d579e4f9604cf8093f17ee557fa4fc84a1ed8cd
Ellis Frank <mooseman1241@protonmail.com>
2021-12-27 11:46:05 -0600

    fix!: can send datagrams, can't deserialize them

--- examples/echo_client.rs
+++ examples/echo_client.rs
@@ -1,26 +1,34 @@
+#[macro_use]
+extern crate log;
+
+use solitude::{DatagramMessage, Session};
+
 use std::net::UdpSocket;
 
-use solitude::{Session, DatagramMessage};
-
 fn main() {
+	env_logger::init();
+
 	let arguments: Vec<String> = std::env::args().collect();
 
 	if arguments.len() < 2 {
 		panic!("must supply I2P hostname, i.e. eva example.i2p");
 	}
 
-	let hostname = arguments[1].clone();
+	let udp_socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+	udp_socket.connect("127.0.0.1:7655").unwrap();
+	let port = udp_socket.local_addr().unwrap().port();
 
-	let socket = UdpSocket::bind("0.0.0.0:0").expect("failed to bind");
-		
-	socket.connect("127.0.0.1:7655").expect("failed to connect to I2P's UDP bridge");
-	
-	let mut session = Session::new("echo_client".to_string(), "0.0.0.0", 0).expect("failed to create Session");
+	let mut session = Session::new("echo_client".to_string(), "0.0.0.0", port).unwrap();
 
-	let destination = session.look_up(hostname).expect("failed to lookup hostname");
-	
-	let message = DatagramMessage::new("echo_client", &destination, String::from("Hello World").into_bytes());
-	let raw = message.serialize();
-	
-	socket.send(&raw).expect("failed to send message");
+	let hostname = arguments[1].to_owned();
+
+	let destination = session.look_up(hostname).unwrap();
+
+	let datagram = DatagramMessage::new("echo_client", &destination, b"Hello World!".to_vec());
+	info!("Sending datagram: {:x?}", datagram);
+
+	let datagram_bytes = datagram.serialize();
+
+	udp_socket.send(&datagram_bytes).unwrap();
+	info!("Sent datagram");
 }
