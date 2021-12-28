c4a718d7d54ec49841bb8a4e902f0bec593dca0e
Ellis Frank <mooseman1241@protonmail.com>
2021-12-27 22:59:07 -0600

    fix: Stopped logging the Session struct itself because it contains the private key

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ impl Session {
 
 		session.bridge(forwarding_address, forwarding_port)?;
 
-		info!("Created new SAMv3 session {:?}", session);
+		info!("Created new SAMv3 session");
 
 		Ok(session)
 	}
