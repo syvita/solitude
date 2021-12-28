a956ec9288e5fb9cf7a17755e275dc108b918964
Linden <70739041+linden@users.noreply.github.com>
2021-12-27 21:50:01 -0800

    fix: account for commands without `RESULT`s

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,7 @@ impl Session {
 			response
 		);
 
-		let expression = regex::Regex::new(r#"(.*) RESULT=(?P<result>[^ ]*) (.*)"#)?;
+		let expression = regex::Regex::new(r#"REPLY(.*)(RESULT=(?P<result>[^ ]*)(.*)|([^\n]*))fi"#)?;
 
 		let matches = expression.captures(&response).context("Could not regex SAMv3's response")?;
 
