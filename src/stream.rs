89d4947f444337ee8c8dc444fcbac2b6abf26698
Linden <70739041+linden@users.noreply.github.com>
2022-01-03 06:12:45 -0800

    style: `cargo fmt`

--- src/stream.rs
+++ src/stream.rs
@@ -2,11 +2,11 @@ use crate::*;
 
 // TODO: add FROM_PORT and TO_PORT
 pub struct StreamInfo {
-    pub destination: String,
+	pub destination: String,
 }
 
 impl StreamInfo {
-    pub fn from_bytes(buffer: &[u8]) -> Result<Self> {
+	pub fn from_bytes(buffer: &[u8]) -> Result<Self> {
 		debug!("deserializing stream info");
 
 		// Split the buffer, using the first 0x0a (newline) byte as the delimiter
@@ -16,18 +16,16 @@ impl StreamInfo {
 
 		let header = String::from_utf8(header_bytes.to_vec())?;
 
-        let expression = regex::Regex::new("^[^ ]+")?;
+		let expression = regex::Regex::new("^[^ ]+")?;
 
-        let destination = expression
-            .captures(&header)
-            .context("Could not regex header")?
-            .get(0)
-            .context("Could not find destination in header")?
-            .as_str()
-            .to_owned();
+		let destination = expression
+			.captures(&header)
+			.context("Could not regex header")?
+			.get(0)
+			.context("Could not find destination in header")?
+			.as_str()
+			.to_owned();
 
-		Ok(Self {
-			destination,
-		})
-    }
+		Ok(Self { destination })
+	}
 }
