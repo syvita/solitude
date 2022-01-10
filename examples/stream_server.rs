da92e4ab5eef1609f0fe9de18a5134c48fcaac87
MooseMan <mooseman1241@protonmail.com>
2022-01-09 17:41:08 -0800

    fix: added logging to examples

--- examples/stream_server.rs
+++ examples/stream_server.rs
@@ -1,5 +1,8 @@
 use std::{io::{BufRead, BufReader}, net::TcpListener};
 
+#[macro_use]
+extern crate log;
+
 use solitude::{Session, SessionStyle, StreamInfo};
 
 use anyhow::Result;
@@ -10,13 +13,16 @@ fn main() -> Result<()> {
 		.parse_env("RUST_LOG")
 		.init();
 
+    info!("Creating tcp server");
     let tcp_listener = TcpListener::bind("127.0.0.1:0")?;
     let port = tcp_listener.local_addr()?.port();
 
+    info!("Creating SAMv3 session");
     let mut session = Session::new("stream_server_example".to_owned(), SessionStyle::Stream)?;
+    info!("Forwarding tcp server to i2p");
     session.forward("127.0.0.1".to_owned(), port)?;
 
-    println!("listening on {}", session.address()?);
+    info!("Listening on {}", session.address()?);
 
     for stream in tcp_listener.incoming() {
         if let Ok(mut stream) = stream {
@@ -26,7 +32,7 @@ fn main() -> Result<()> {
             let mut data = String::new();
             reader.read_line(&mut data)?;
             
-            println!("received: \"{}\" from {}", data, stream_info.destination);
+            info!("received: \"{}\" from {}", data, stream_info.destination);
         }
     }
 
