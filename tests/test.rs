2b22bf07ecab403e00a8e320403a47547e2ff28f
Ellis Frank <mooseman1241@protonmail.com>
2022-01-09 17:50:10 -0800

    fix: removed unneeded imports

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,6 @@
-use solitude::{DatagramMessage, Session, SessionStyle};
+use solitude::{Session, SessionStyle};
 
-use std::net::UdpSocket;
-
-use anyhow::{Context, Result};
+use anyhow::Result;
 
 fn init() {
 	let _ = env_logger::builder().is_test(true).format_module_path(true).try_init();
