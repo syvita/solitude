fdb8cb8cbeaee7d01616ff729898e41de0ad0253
MooseMan <mooseman1241@protonmail.com>
2022-01-09 16:57:52 -0800

    fix: echo server and client work now

--- examples/echo_server.rs
+++ examples/echo_server.rs
@@ -5,44 +5,47 @@ use solitude::{DatagramMessage, Session};
 
 use std::net::UdpSocket;
 
-fn main() {
-	// env_logger::builder()
-	// 	.filter_level(log::LevelFilter::Info)
-	// 	.parse_env("RUST_LOG")
-	// 	.init();
+use anyhow::Result;
 
-	// let udp_socket = UdpSocket::bind("127.0.0.1:0").unwrap();
-	// udp_socket.connect("127.0.0.1:7655").unwrap();
+fn main() -> Result<()> {
+	env_logger::builder()
+		.filter_level(log::LevelFilter::Info)
+		.parse_env("RUST_LOG")
+		.init();
 
-	// let port = udp_socket.local_addr().unwrap().port();
+	let udp_socket = UdpSocket::bind("127.0.0.1:0")?;
+	udp_socket.connect("127.0.0.1:7655")?;
 
-	// let session = Session::new(String::from("echo_server"), "127.0.0.1", port).unwrap();
+	let port = udp_socket.local_addr()?.port();
 
-	// info!("Listening on i2p at {}", session.address().unwrap());
+	let mut session = Session::new(String::from("echo_server"), solitude::SessionStyle::Datagram)?;
+    session.forward("127.0.0.1".to_owned(), port)?;
 
-	// let mut buffer = [0u8; 2048];
+	info!("Listening on i2p at {}", session.address()?);
 
-	// loop {
-	// 	info!("Waiting to receive");
-	// 	let frame = udp_socket.recv(&mut buffer).unwrap();
-	// 	let buffer = &mut buffer[..frame];
+	let mut buffer = [0u8; 2048];
 
-	// 	debug!("Received datagram bytes: {:02x?}", buffer);
+	loop {
+		info!("Waiting to receive");
+		let frame = udp_socket.recv(&mut buffer)?;
+		let buffer = &mut buffer[..frame];
 
-	// 	let received_datagram = match DatagramMessage::from_bytes("echo_server", &buffer) {
-	// 		Ok(received_datagram) => received_datagram,
-	// 		Err(_) => {
-	// 			debug!("Received a datagram but could not deserialize it");
-	// 			continue;
-	// 		}
-	// 	};
+		debug!("Received datagram bytes: {:02x?}", buffer);
 
-	// 	info!("Received datagram: {}", std::str::from_utf8(&received_datagram.contents).unwrap());
+		let received_datagram = match DatagramMessage::from_bytes("echo_server", &buffer) {
+			Ok(received_datagram) => received_datagram,
+			Err(_) => {
+				debug!("Received a datagram but could not deserialize it");
+				continue;
+			}
+		};
 
-	// 	let datagram_to_send = DatagramMessage::new("echo_server", &received_datagram.destination, received_datagram.contents);
+		info!("Received datagram: {}", std::str::from_utf8(&received_datagram.contents)?);
 
-	// 	let datagram_to_send_bytes = datagram_to_send.serialize();
+		let datagram_to_send = DatagramMessage::new("echo_server", &received_datagram.destination, received_datagram.contents);
 
-	// 	udp_socket.send(&datagram_to_send_bytes).unwrap();
-	// }
+		let datagram_to_send_bytes = datagram_to_send.serialize();
+
+		udp_socket.send(&datagram_to_send_bytes)?;
+	}
 }
